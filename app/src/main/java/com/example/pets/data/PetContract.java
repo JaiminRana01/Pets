17c6ba8eced3b9cca391c4cb2b8273a87ecfcfb9
JaiminRana01 <jaiminr625@gmail.com>
2021-03-27 23:59:06 +0530

    contract

---
+++ app/src/main/java/com/example/pets/data/PetContract.java
@@ -0,0 +1,17 @@
+package com.example.pets.data;
+
+import android.provider.BaseColumns;
+
+public final class PetContract {
+    public static final class PetEntry implements BaseColumns {
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+}

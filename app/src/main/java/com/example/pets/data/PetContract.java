1c9309f0a3f3aa29a4bf2a3a1d751ded06f3dabb
JaiminRana01 <jaiminr625@gmail.com>
2021-03-28 19:33:33 +0530

    2.2

--- app/src/main/java/com/example/pets/data/PetContract.java
+++ app/src/main/java/com/example/pets/data/PetContract.java
@@ -2,13 +2,68 @@ package com.example.pets.data;
 
 import android.provider.BaseColumns;
 
+/**
+ * API Contract for the Pets app.
+ */
 public final class PetContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private PetContract() {
+    }
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
     public static final class PetEntry implements BaseColumns {
+
+        /**
+         * name of the database table for pets
+         */
+        public static final String TABLE_NAME = "pets";
+
+        /**
+         * Unique ID number for the pet (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
         public static final String _ID = BaseColumns._ID;
+
+        /**
+         * name of the pet.
+         *
+         * Type: TEXT
+         */
         public static final String COLUMN_PET_NAME = "name";
+
+        /**
+         * breed of the pet.
+         *
+         * Type: TEXT
+         */
         public static final String COLUMN_PET_BREED = "breed";
+
+        /**
+         * gender of the pet.
+         *
+         * The only possible values are {@link #GENDER_MALE}, {@link #GENDER_FEMALE}
+         * or {@link #GENDER_UNKNOWN}.
+         *
+         * Type: INTEGER
+         */
         public static final String COLUMN_PET_GENDER = "gender";
+
+        /**
+         * weight of the pet.
+         *
+         * Type: INTEGER
+         */
         public static final String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * possible values for gender of the pets.
+         */
 
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
---
+++ app/src/main/java/com/example/pets/data/PetDbHelper.java
@@ -0,0 +1,59 @@
+package com.example.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.pets.data.PetContract.PetEntry;
+
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG = PetDbHelper.class.getSimpleName();
+
+    /**
+     * Name of the database file
+     */
+    public static final String DATABASE_NAME = "shelter.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * constructs a new instance of {@link PetDbHelper}.
+     *
+     * @param context of the app.
+     */
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * this is called when database is created first time.
+     */
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL,"
+                + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+        //execute the sql statement.
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+
+    }
+
+    /**
+     * this called when database needs to be upgraded.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
